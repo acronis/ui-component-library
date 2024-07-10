4117440ccf767cf60f12027dcd07626a1199ec6e
Leonid Romanov <romanov.leonid@gmail.com>
2024-07-10 15:24:02 +0300

    fix(form): implement simple form item

--- packages/ui/src/components/form-item/formItem.vue
+++ packages/ui/src/components/form-item/formItem.vue
@@ -1,32 +1,67 @@
 <script setup lang="ts">
+  import { computed, defineProps, defineSlots } from 'vue';
   import type {
     AcvFormItemEvents,
     AcvFormItemProps,
     AcvFormItemSlots
   } from './formItem.ts';
+  import AcvTypography from '@/components/typography/typography.vue';
   import './formItem.css';
 
-  const { title, description } = defineProps<AcvFormItemProps>();
+  const { title, control, titlePlacement } = defineProps<AcvFormItemProps>();
 
   defineEmits<AcvFormItemEvents>();
   defineSlots<AcvFormItemSlots>();
+
+  const formItemClasses = computed(() => ({
+    'acv-form-item': true,
+    [titlePlacement]: titlePlacement,
+  }));
 </script>
 
 <template>
-  <div class="acv-form-item">
-    <slot>{{ title }}</slot>
-    <slot
-      name="description"
-      :description="description"
+  <label :class="formItemClasses">
+    <control v-bind="$attrs" />
+    <AcvTypography
+      variant="body.large"
+      component="span"
+      :disabled="$attrs.disabled"
     >
-      {{ description }}
-    </slot>
-  </div>
+      <slot>
+        {{ title }}
+      </slot>
+    </AcvTypography>
+  </label>
 </template>
 
 <style scoped>
   .acv-form-item {
-    font-weight: var(--acv-font-weight-strong);
-    color: var(--acv-form-item-color);
+    display: inline-flex;
+    user-select: none;
+    flex-wrap: nowrap;
+    align-items: center;
+    cursor: pointer;
+
+    &.top {
+      flex-direction: column-reverse;
+    }
+
+    &.bottom {
+      flex-direction: column;
+    }
+
+    &.left {
+      flex-direction: row-reverse;
+    }
+
+    &.right {
+      flex-direction: row;
+    }
+
+    &:has(:required):after {
+      content: '*';
+      color: var(--acv-color-danger);
+      margin-left: 4px;
+    }
   }
 </style>
