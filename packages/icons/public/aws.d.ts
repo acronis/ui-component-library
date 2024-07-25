76b974e5490b4cfafe9425dfb0831eef224d6f64
Viacheslav Kozorezov <viacheslav.kozorezov@constructor.tech>
2024-07-25 11:50:44 +0300

    feat(icon): review fixes

--- packages/icons/public/aws.d.ts
+++ packages/icons/public/aws.d.ts
@@ -1,2 +1,2 @@
-export { default as IconAws32 } from '../vue/IconAws32.vue';
 export { default as IconAws24 } from '../vue/IconAws24.vue';
+export { default as IconAws32 } from '../vue/IconAws32.vue';
--- packages/icons/public/reverse.js
+++ packages/icons/public/reverse.js
@@ -1,2 +1,2 @@
-export { default as IconReverse16 } from '../vue/IconReverse16.vue';
 export { default as IconReverseO16 } from '../vue/IconReverseO16.vue';
+export { default as IconReverse16 } from '../vue/IconReverse16.vue';
