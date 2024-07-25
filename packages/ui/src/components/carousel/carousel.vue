471d1efa0eaa049fc69ee3e82926faa63321f5bb
Radik Aksyanov <Radik.Aksyanov@acronis.com>
2024-07-25 17:58:00 +0300

    feat(carousel): carousel component first iteration

--- packages/ui/src/components/carousel/carousel.vue
+++ packages/ui/src/components/carousel/carousel.vue
@@ -1,21 +1,68 @@
 <script setup lang="ts">
-  import type { AcvCarouselEvents, AcvCarouselProps, AcvCarouselSlots } from './carousel.ts';
+  import { computed } from 'vue';
+  import AcvButton from '../button/button.vue';
+  import type { AcvCarouselProps, AcvCarouselSlots } from './carousel.ts';
   import './carousel.css';
 
-  const { title, description } = defineProps<AcvCarouselProps>();
+  const { count, cycle } = withDefaults(defineProps<AcvCarouselProps>(), {
+    cycle: false
+  });
 
-  defineEmits<AcvCarouselEvents>();
   defineSlots<AcvCarouselSlots>();
+  const model = defineModel<number>({ default: 0 });
+
+  const isFirst = computed(() => model.value === 0);
+  const isLast = computed(() => model.value === (count - 1));
+  const isFirstDisabled = computed(() => !cycle && isFirst.value);
+  const isLastDisabled = computed(() => !cycle && isLast.value);
+
+  function setActive(index: number) {
+    model.value = index;
+  }
+
+  function prev() {
+    setActive((model.value + count - 1) % count);
+  }
+
+  function next() {
+    setActive((model.value + 1) % count);
+  }
 </script>
 
 <template>
   <div class="acv-carousel">
-    <slot>{{ title }}</slot>
     <slot
-      name="description"
-      :description="description"
+      name="prev"
+      :prev="prev"
+      :disabled="isFirstDisabled"
     >
-      {{ description }}
+      <AcvButton
+        :disabled="isFirstDisabled"
+        @click="prev"
+      >
+        Prev
+      </AcvButton>
+    </slot>
+    <AcvButton
+      v-for="(_, $index) in count"
+      :key="$index"
+      :disabled="$index === model"
+      variant="ghost"
+      @click="setActive($index)"
+    >
+      *
+    </AcvButton>
+    <slot
+      name="next"
+      :next="next"
+      :disabled="isLastDisabled"
+    >
+      <AcvButton
+        :disabled="isLastDisabled"
+        @click="next"
+      >
+        Next
+      </AcvButton>
     </slot>
   </div>
 </template>
