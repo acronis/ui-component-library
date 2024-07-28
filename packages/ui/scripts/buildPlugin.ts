c2e162351d911ba6be308bd32ec5ff5678592a89
Leonid Romanov <romanov.leonid@gmail.com>
2024-07-29 00:46:50 +0300

    chore: fix build aliases

--- packages/ui/scripts/buildPlugin.ts
+++ packages/ui/scripts/buildPlugin.ts
@@ -28,7 +28,8 @@ export function buildPlugin(): Plugin[] {
       compilerOptions: {
         sourceMap: false,
         paths: {
-          '@/*': ['./*'],
+          '@/*': ['./src/*'],
+          '@demos/*': ['../../demos/*'],
           'csstype': ['node_modules/csstype']
         }
       },
