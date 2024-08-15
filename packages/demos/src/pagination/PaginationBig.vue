6f0a175f689f9a1f862a90793c2715461abd4e9e
Viacheslav Kozorezov <viacheslav.kozorezov@constructor.tech>
2024-08-15 15:21:28 +0300

    feat(pagination): create pagination component

---
+++ packages/demos/src/pagination/PaginationBig.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import { AcvPagination, usePagination } from '@acronis-platform/ui-component-library';
+
+  const paginationService = usePagination({ limit: 1 });
+  const { setTotal, setPage } = paginationService;
+
+  setTotal(20);
+  setPage(1);
+</script>
+
+<template>
+  <AcvPagination :pagination="paginationService" />
+</template>
