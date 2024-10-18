536abb50d3b47d24e6e6bdf7ae289560c1d6f338
Viktor Atanasov <viktor.atanasov@constructor.tech>
2024-10-18 17:51:11 +0300

    feat: add functional breadcrumbs component

--- packages/demos/src/breadcrumbs/BreadcrumbsCharacterSeparator.vue
+++ packages/demos/src/breadcrumbs/BreadcrumbsCharacterSeparator.vue
@@ -1,12 +1,10 @@
 <script setup>
   import BreadcrumbItem from '@/components/breadcrumbs/breadcrumbItem.vue';
   import Breadcrumbs from '@/components/breadcrumbs/breadcrumbs.vue';
-  // import { IconChevronRight16 } from '@acronis-platform/icons/chevron';
-  import { IconArrowRight16 } from '@acronis-platform/icons/arrow';
 </script>
 
 <template>
-  <Breadcrumbs :separator-icon="IconArrowRight16">
+  <Breadcrumbs separator="/">
     <BreadcrumbItem to="/">
       Home
     </BreadcrumbItem>
--- packages/ui/src/components/breadcrumbs/public.ts
+++ packages/ui/src/components/breadcrumbs/public.ts
@@ -1,3 +1,5 @@
+export { default as AcvBreadcrumbItem } from './breadcrumbItem.vue';
 export { default as AcvBreadcrumbLink } from './breadcrumbLink.vue';
 export * from './breadcrumbs.ts';
 export { default as AcvBreadcrumbs } from './breadcrumbs.vue';
+export { default as AcvBreadcrumbSection } from './breadcrumbSection.vue';
