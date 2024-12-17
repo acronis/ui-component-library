ebea29ad2fa5cffb77e10b742a0c8afb53cc9ca5
Leonid Romanov <romanov.leonid@gmail.com>
2024-12-17 14:43:46 +0200

    fix: fix Regular expression injection

--- packages/ui/scripts/utils/fuzzyMatch.ts
+++ packages/ui/scripts/utils/fuzzyMatch.ts
@@ -1,9 +1,13 @@
+import { escapeRegExp } from 'lodash-es';
+
 export function fuzzyMatch(partials: string[], total: string[], includeAll = false) {
   const matched: string[] = [];
 
   partials.forEach((partial) => {
+    const safePartial = escapeRegExp(partial);
+
     for (const target of total) {
-      if (target.match(partial)) {
+      if (target.match(safePartial)) {
         matched.push(target);
 
         if (!includeAll) {
