1032e7543994dec82e2c19573d0977618e87e461
Leonid Romanov <romanov.leonid@gmail.com>
2025-01-14 18:09:22 +0200

    feat: update icons from figma

---
+++ packages/icons/public/proxmox.d.ts
@@ -0,0 +1,4 @@
+export { default as IconProxmox16 } from '../vue/IconProxmox16.js';
+export { default as IconProxmox32 } from '../vue/IconProxmox32.js';
+export { default as IconProxmoxVm32 } from '../vue/IconProxmoxVm32.js';
+export { default as IconProxmoxVmRunning32 } from '../vue/IconProxmoxVmRunning32.js';
---
+++ packages/icons/public/proxmox.js
@@ -0,0 +1,4 @@
+export { render as IconProxmox16 } from '../vue/IconProxmox16.js';
+export { render as IconProxmox32 } from '../vue/IconProxmox32.js';
+export { render as IconProxmoxVm32 } from '../vue/IconProxmoxVm32.js';
+export { render as IconProxmoxVmRunning32 } from '../vue/IconProxmoxVmRunning32.js';
