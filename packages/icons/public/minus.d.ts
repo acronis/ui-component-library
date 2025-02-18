86f3ab4289625553f1d4740bcc0b663ab5ff01ee
Leonid Romanov <romanov.leonid@gmail.com>
2025-02-18 16:46:24 +0200

    feat: update icons from figma

--- packages/icons/public/minus.d.ts
+++ packages/icons/public/minus.d.ts
@@ -1,1 +1,3 @@
+export { default as IconMinus16 } from '../vue/IconMinus16.js';
 export { default as IconMinusRounded16 } from '../vue/IconMinusRounded16.js';
+export { default as IconMinusSmall16 } from '../vue/IconMinusSmall16.js';
--- packages/icons/public/minus.js
+++ packages/icons/public/minus.js
@@ -1,1 +1,3 @@
+export { render as IconMinus16 } from '../vue/IconMinus16.js';
 export { render as IconMinusRounded16 } from '../vue/IconMinusRounded16.js';
+export { render as IconMinusSmall16 } from '../vue/IconMinusSmall16.js';
